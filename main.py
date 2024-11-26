be2d2311d04f019ab116621ef38a5a531f3b13d4
jhon <jfinfocesde@gmail.com>
2024-11-26 18:02:09 -0500

    Generar archivo .csv con la fecha y hora en el nombre

--- main.py
+++ main.py
@@ -37,12 +37,17 @@ def scrape_falabella(url_base, num_pages=10):
         except requests.exceptions.RequestException as e:
             logging.error(f"Error al obtener la página {url}: {e}")
             break # Detener el scraping si hay un error de red
-       
-    return pd.DataFrame(data)
+        
+    df = pd.DataFrame(data)    
+    from datetime import datetime
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    df.to_csv(f"laptops_{timestamp}.csv", index=False)
+
+    return df
 
 
 if __name__ == "__main__":
     url_base = 'https://www.falabella.com.co/falabella-co/category/cat1361001/Computadores-Portatiles'
     df = scrape_falabella(url_base)
     print(df)
-    df.to_csv("laptops.csv", index=False)
+    # df.to_csv("laptops.csv", index=False)
